328e8ea96afe6241346b0dd444b7e484bb5e6f19
Icchan-27 <christianusmn@gmail.com>
2024-12-05 22:39:38 +0800

    Add files via upload

--- Log_in.py
+++ Log_in.py
@@ -1,38 +1,102 @@
-import Space_impact as S_I
+import tkinter as tk
+from tkinter import messagebox as mb
+import customtkinter as ctk
+import Game as SI
 
-class Log_in():
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+class Account_Entry:
     def __init__(self):
-        self.user_email="user"
-        self.user_pass="user123"
-        self.admin_email="admin"
-        self.admin_pass="admin123"
+        self.user_account = "user"
+        self.user_password = "user123"
+        self.admin_account = "admin"
+        self.admin_password = "admin123"
 
-    def user_login(self, email, password):
-        if email == self.user_email and password == self.user_pass:
-            S_I.play()
+    def user_click(self):
+        user_login = tk.Toplevel()
+        user_login.title("User Login")
+        user_login.geometry("500x300")
+        user_login.configure(bg="#008080")  
+
+        tk.Label(user_login, text="Username:", bg="#008080", fg="white").place(x=100, y=80)
+        User_account = tk.Entry(user_login)
+        User_account.place(x=200, y=80)
+
+        tk.Label(user_login, text="Password:", bg="#008080", fg="white").place(x=100, y=120)
+        User_password = tk.Entry(user_login, show="*")
+        User_password.place(x=200, y=120)
+
+        login_button = tk.Button(user_login, text="Login", command=lambda: self.user_login(User_account.get(), User_password.get()))
+        login_button.place(x=220, y=160)
+
+    def user_login(self, User_account, User_password):
+        if User_account == self.user_account and User_password == self.user_password:
+            SI.play()
         else:
-            print("Email or password is incorrect.")
-            print()
+            mb.showinfo("Can't Log In", "Account or password incorrect")
 
-    def admin_login(self, email, password):
-        if email == self.user_email and password == self.user_pass:
-            print("Sawadheeka")
+    def admin_click(self):
+        admin_login = tk.Toplevel()
+        admin_login.title("Admin Login")
+        admin_login.geometry("500x300")
+        admin_login.configure(bg="#008080")  
+
+        tk.Label(admin_login, text="Username:", bg="#008080", fg="white").place(x=100, y=80)
+        Admin_account = tk.Entry(admin_login)
+        Admin_account.place(x=200, y=80)
+
+        tk.Label(admin_login, text="Password:", bg="#008080", fg="white").place(x=100, y=120)
+        Admin_password = tk.Entry(admin_login, show="*")
+        Admin_password.place(x=200, y=120)
+
+        login_button = tk.Button(admin_login, text="Login", command=lambda: self.admin_login(Admin_account.get(), Admin_password.get()))
+        login_button.place(x=220, y=160)
+
+    def admin_login(self, Admin_account, Admin_password):
+        if Admin_account == self.admin_account and Admin_password == self.admin_password:
+            mb.showinfo("Login", "Admin logged in successfully!")
         else:
-            print("Email or password is incorrect.")
-            print()
+            mb.showinfo("Can't Log In", "Account or password incorrect")
 
-login=Log_in()
-while True:
-    print("---------------------------------------------- SELECT -------------------------------------------------")
-    print("                  1. USER                        |                         2. ADMIN")
-    choice=int(input("\nCHOOSE:  "))
+class Login_Interface:
+    entry = Account_Entry()
+    
+    def __init__(self):
+        self.interface = tk.Tk()
+        self.interface.title("Select")
+        self.interface.geometry("500x700")
+        self.interface.configure(bg="#008080")  
 
-    if choice==1:
-        email=input("Enter user email: ")
-        password=input("Enter user password: ")
-        login.user_login(email, password)
+        self.create_button("user", 0.4, self.user_click)
+        self.create_button("admin", 0.6, self.admin_click)
 
-    elif choice==1:
-        email=input("Enter user email: ")
-        password=input("Enter user password: ")
-        login.admin_login(email, password)
+    def create_button(self, text, rel_y, command):
+        
+        button = ctk.CTkButton(
+            self.interface, 
+            text=text, 
+            font=("Arial", 20), 
+            fg_color="#03313d",       
+            hover_color="#044a5a",    
+            text_color="white",       
+            command=command,
+            width=200,
+            height=50,
+            corner_radius=20,         
+            border_width=0            
+        )
+        button.place(relx=0.5, rely=rel_y, anchor="center")
+
+    def user_click(self):
+        self.entry.user_click()
+
+    def admin_click(self):
+        self.entry.admin_click()
+
+    def run(self):
+        self.interface.mainloop()
+
+interface = Login_Interface()
+interface.run()
--- Space_impact.py
+++ Space_impact.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox as mb
-from PIL import Image, ImageTk
 import random as R
 
 def play():
@@ -11,94 +9,80 @@ class Game():
         self.Interface = tk.Tk()
         self.Interface.geometry("500x650")
         self.Interface.title("Space Impact")
-        self.score = 0
         
-        self.canvas = tk.Canvas(self.Interface, width=500, height=670)
+        self.canvas = tk.Canvas(self.Interface, width=500, height=670, bg="black")
         self.canvas.pack()
-        
-        self.Space = ImageTk.PhotoImage(Image.open(r"C:\Users\chris\Pictures\smoother_pixelated_image.jpg").resize((500, 670)))
-        self.Main_player = ImageTk.PhotoImage(Image.open(r"C:\Users\chris\Pictures\transparent_ufo.png").resize((125, 120)))
-        self.enemy_image = ImageTk.PhotoImage(Image.open(r"C:\Users\chris\Pictures\ship_image.png").resize((100, 100)))
-        self.bullet_image = ImageTk.PhotoImage(Image.open(r"C:\Users\chris\Pictures\new_bullet.png").resize((10, 25)))
-        self.explosion_image = ImageTk.PhotoImage(Image.open(r"C:\Users\chris\Pictures\explosion-removebg-preview.png").resize((100, 100)))
-        
-        self.canvas.create_image(250, 335, image=self.Space)
-        self.score_text = self.canvas.create_text(50, 20, text=f"Score: {self.score}", fill="white", font=("Arial", 16))
-        self.character_id = self.canvas.create_image(250, 610, image=self.Main_player)
-        self.enemies = []
-        
-        self.spawn_enemy()
-        self.canvas.bind_all("<Left>", lambda e: self.move_player(-10))
-        self.canvas.bind_all("<Right>", lambda e: self.move_player(10))
+
+        self.ship = self.canvas.create_polygon(194, 633, 223, 596, 236, 546, 249, 596, 279, 633, 250, 633, 250, 638, 243, 638, 243, 633, 230, 633, 230, 638, 222, 638, 222, 633, fill="#790D0D")
+
+        self.canvas.bind_all("<Left>", self.move_left)
+        self.canvas.bind_all("<Right>", self.move_right)
         self.canvas.bind_all("<space>", self.shoot)
 
-    def move_player(self, dx):
-        x, _ = self.canvas.coords(self.character_id)
-        if 50 <= x + dx <= 450:
-            self.canvas.move(self.character_id, dx, 0)
+        self.enemies = []
+        self.create_enemy()
 
-    def shoot(self, _):
-        x, y = self.canvas.coords(self.character_id)
-        bullet_id = self.canvas.create_image(x, y, image=self.bullet_image, anchor="center")
-        self.animate_bullet(bullet_id)
+    def move_left(self, event):
+        x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13 = self.canvas.coords(self.ship)
+        if x1 > 10:
+            self.canvas.move(self.ship, -10, 0)
 
-    def animate_bullet(self, bullet_id):
-        if self.canvas.coords(bullet_id):
-            y = self.canvas.coords(bullet_id)[1]
-            if y > 0:
-                self.canvas.move(bullet_id, 0, -10)
-                self.check_collision(bullet_id)
-                self.Interface.after(80, lambda: self.animate_bullet(bullet_id))
+    def move_right(self, event):
+        x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13 = self.canvas.coords(self.ship)
+        if x5 < 490:
+            self.canvas.move(self.ship, +10, 0)
+
+    def shoot(self, event):
+        x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13 = self.canvas.coords(self.ship)
+        bullet_id = self.canvas.create_oval(x3 - 5, y3 - 10, x3 + 3, y3 +10, fill="blue")
+        self.move_bullet(bullet_id)
+
+    def move_bullet(self, bullet_id):
+        coords = self.canvas.coords(bullet_id)
+        if coords:
+            x1, y1, x2, y2 = coords
+            if y2 > 0: 
+                self.canvas.move(bullet_id, 0, -10)  
+                self.check_collision(bullet_id) 
+                self.Interface.after(60, lambda: self.move_bullet(bullet_id)) 
             else:
                 self.canvas.delete(bullet_id)
 
     def check_collision(self, bullet_id):
         if self.canvas.coords(bullet_id):
-            bx, by = self.canvas.coords(bullet_id)
+            bullet_x1, bullet_y1, bullet_x2, bullet_y2 = self.canvas.coords(bullet_id)
             for enemy_id in self.enemies[:]:
-                ex, ey = self.canvas.coords(enemy_id)
-                if abs(bx - ex) < 50 and abs(by - ey) < 50:
-                    self.destroy_objects(bullet_id, enemy_id)
-                    self.score += 1
-                    self.canvas.itemconfig(self.score_text, text=f"Score: {self.score}")
+                enemy_x1, enemy_y1, enemy_x2, enemy_y2, enemy_x3, enemy_y3, enemy_x4, enemy_y4, enemy_x5, enemy_y5, enemy_x6, enemy_y6, enemy_x7, enemy_y7, enemy_x8, enemy_y8, enemy_x9, enemy_y9, enemy_x10, enemy_y10, enemy_x11, enemy_y11, enemy_x12, enemy_y12 = self.canvas.coords(enemy_id)
+                if (bullet_x2 > enemy_x1 and bullet_x1 < enemy_x7) and (bullet_y2 < enemy_y8 and bullet_y1 > enemy_y2):
+                    self.canvas.delete(bullet_id)
+                    self.canvas.delete(enemy_id)
+                    self.enemies.remove(enemy_id)
 
-    def spawn_enemy(self):
-        x = R.randint(50, 450)
-        enemy_id = self.canvas.create_image(x, 50, image=self.enemy_image, anchor="center")
+    def create_enemy(self):
+        x1 = R.randint(20, 450)
+        enemy_id = self.canvas.create_polygon(x1, 42, x1 + 13, 25, x1 + 13, 46, x1 + 23, 28, x1 + 34, 46, x1 + 34, 25, x1 + 48, 42, x1 + 34, 93, x1 + 34, 63, x1 + 24, 81, x1 + 14, 63, x1 + 13, 93, fill="gray")
         self.enemies.append(enemy_id)
-        self.move_enemy(enemy_id)
-        self.Interface.after(1000, self.spawn_enemy)
+        self.move_enemies(enemy_id)
+        self.Interface.after(800, self.create_enemy)
 
-    def move_enemy(self, enemy_id):
+    def move_enemies(self, enemy_id):
         if self.canvas.coords(enemy_id):
-            y = self.canvas.coords(enemy_id)[1]
-            if y < 670:
-                self.canvas.move(enemy_id, 0, 10)
+            x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12 = self.canvas.coords(enemy_id)
+            if y8 < 650:
+                self.canvas.move(enemy_id, 0, +10)
                 self.check_player_collision(enemy_id)
-                self.Interface.after(100, lambda: self.move_enemy(enemy_id))
+                self.Interface.after(80, lambda: self.move_enemies(enemy_id))
             else:
-                self.enemies.remove(enemy_id)
                 self.canvas.delete(enemy_id)
+                self.enemies.remove(enemy_id)
 
     def check_player_collision(self, enemy_id):
-        ex, ey = self.canvas.coords(enemy_id)
-        px, py = self.canvas.coords(self.character_id)
-        if abs(px - ex) < 50 and abs(py - ey) < 50:
-            self.destroy_objects(enemy_id, None)
-            self.end_game()
-
-    def destroy_objects(self, *ids):
-        for obj_id in ids:
-            if obj_id:
-                self.canvas.delete(obj_id)
-                if obj_id in self.enemies:
-                    self.enemies.remove(obj_id)
-
-    def end_game(self):
-        x, y = self.canvas.coords(self.character_id)
-        self.canvas.create_image(x, y, image=self.explosion_image)
-        self.Interface.after(500, lambda: self.Interface.destroy())
-        mb.showinfo("Game Over", "You have been destroyed!")
+        enemy_x1, enemy_y1, enemy_x2, enemy_y2, enemy_x3, enemy_y3, enemy_x4, enemy_y4, enemy_x5, enemy_y5, enemy_x6, enemy_y6, enemy_x7, enemy_y7, enemy_x8, enemy_y8, enemy_x9, enemy_y9, enemy_x10, enemy_y10, enemy_x11, enemy_y11, enemy_x12, enemy_y12 = self.canvas.coords(enemy_id)
+        x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13 = self.canvas.coords(self.ship)
+        if (x5 > enemy_x1 and x1 < enemy_x7) and (y7 < enemy_y8 and y3 > enemy_y2):
+            self.canvas.delete(enemy_id)
+            self.enemies.remove(enemy_id)
+            self.Interface.destroy()
 
     def run(self):
         self.Interface.mainloop()
